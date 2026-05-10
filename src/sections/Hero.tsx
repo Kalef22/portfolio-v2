07f408edf46fd356f456bf0db2318782d71b3dbb
Kalef22 <144925042+Kalef22@users.noreply.github.com>
2026-05-10 16:35:23 +0200

    feat: añadir enlaces sociales y botón de CV

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 function Hero() {
     return (
         <section id="hero" className="hero reveal">
@@ -12,12 +13,33 @@ function Hero() {
             </p>
 
             <div className="hero-buttons">
-                <a className="button" href="#proyectos">
+                <a href="#proyectos" className="primary-button">
                     Ver proyectos
                 </a>
 
-                <a className="button secondary" href="#contacto">
+                <a href="#contacto" className="secondary-button">
                     Contactar
+                </a>
+            </div>
+
+            <div className="hero-socials">
+                <a
+                    href="https://github.com/Kalef22"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaGithub />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/kaleff-villanueva-desarrollador-web-aws"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaLinkedin />
+                </a>
+
+                <a href="/cv.pdf" className='cv-button'>
+                    Descargar CV
                 </a>
             </div>
         </section>
